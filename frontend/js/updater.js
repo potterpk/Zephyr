c3e56751ff4e76a0e60e853223f9913b9294ce73
potterpk <essarjper207@gmail.com>
2026-06-22 15:32:46 +0300

    they now move

--- frontend/js/updater.js
+++ frontend/js/updater.js
@@ -1,20 +1,14 @@
-import map from './map.js'
 import { loadFlights } from './flights.js'
 import { renderFlights } from './markers.js'
-import { closePanel } from './panels.js'
-import { startUpdater } from './updater.js'
-import './theme.js'
-
-window.closePanel = closePanel
 
 const stats = document.getElementById('stats-bar')
 
-async function init() {
-    stats.textContent = 'loading...'
+async function refresh() {
     const flights = await loadFlights()
     renderFlights(flights)
     stats.textContent = `${flights.length} flights in the air`
-    startUpdater()
 }
 
-init()
+export function startUpdater() {
+    setInterval(refresh, 10000)
+}
